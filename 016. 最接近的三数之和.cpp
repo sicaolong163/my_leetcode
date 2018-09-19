bfa6f0a63fbea55c771cd29facd8f925dccf62fa
sicaolong163 <39057759+sicaolong163@users.noreply.github.com>
2018-09-19 22:02:35 +0800

    Update 016. 最接近的三数之和.cpp

--- 016. 最接近的三数之和.cpp
+++ 016. 最接近的三数之和.cpp
@@ -1,25 +1,32 @@
 class Solution {
+    //思路：
+    /*
+        1、排序
+        2、去重、
+        3、双索引求和
+        4、比较大小；
+    */
 public:
     int threeSumClosest(vector<int>& nums, int target) 
     {
         const int n=nums.size();
         assert(n>=3);
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end());//1、排序
         int res=nums[0]+nums[1]+nums[n-1];//初始值；
         for(int i=0;i<n;i++)
         {
-            if(i>0&&nums[i]==nums[i-1])
+            if(i>0&&nums[i]==nums[i-1])//2、去重
                 continue;
             int left=i+1;
             int right=n-1;
-            while(left<right)
+            while(left<right)//3双索引求和
             {
                 int sum=nums[i]+nums[left]+nums[right];
                 if(sum>target)
                     right--;
                 else
                     left++;
-                if(abs(sum-target)<abs(res-target))
+                if(abs(sum-target)<abs(res-target))//4比较大小
                     res=sum;
             }
         }
